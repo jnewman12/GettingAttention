f44c2e9625ca2032ac1a891a786b132d71e48eb5
James Newman <jamesmnewman1@gmail.com>
2014-04-08 23:46:03 -0700

    inital commit from local git

---
+++ GettingAttention/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GettingAttention
+//
+//  Created by James Newman on 4/8/14.
+//  Copyright (c) 2014 James Newman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GettingAttention/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GettingAttention
+//
+//  Created by James Newman on 4/8/14.
+//  Copyright (c) 2014 James Newman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UIAlertViewDelegate>
+
+@end
